fad1c252acc341d8967fc434103c65d687443386
Pratik Agarwal <pratiknator44@gmail.com>
2023-12-25 15:35:21 +0530

    removed carousel text

--- src/components/home/home-carousel.component.tsx
+++ src/components/home/home-carousel.component.tsx
@@ -6,26 +6,18 @@ export default function HomeCarouselComponent() {
             <Carousel>
                 <Carousel.Item>
                     <img height={300} src='https://kajabi-storefronts-production.kajabi-cdn.com/kajabi-storefronts-production/blogs/18725/images/PuRv7TFRzGdNsmp3iRmj_Tetons.jpg' />
-                    <Carousel.Caption>
+                    {/* <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
                     <img height={300} src='https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/blogs/18725/images/IjBZwEKWSXmRkHRd1DnE_MountKiddSilhouette.jpg' />
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
+
                 </Carousel.Item>
                 <Carousel.Item>
                     <img height={300} src='https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/blogs/18725/images/gga5HfWGQWSiO3udSRgl_Tetons.jpg' />
-                    <Carousel.Caption>
-                        <h3>Third slide label</h3>
-                        <p>
-                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                        </p>
-                    </Carousel.Caption>
+
                 </Carousel.Item>
             </Carousel>
         </div>
--- src/components/home/wish-category-items.component.tsx
+++ src/components/home/wish-category-items.component.tsx
@@ -21,10 +21,10 @@ export default function WishCategoryItemsComponent() {
                         </div>
                     </div>
 
-                    <Carousel.Caption>
+                    {/* <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                    </Carousel.Caption>
+                    </Carousel.Caption> */}
                 </Carousel.Item>
                 <Carousel.Item>
                     <div className='d-flex' style={{ justifyContent: 'space-evenly' }}>
@@ -43,10 +43,6 @@ export default function WishCategoryItemsComponent() {
                             <div className='bg-white text-dark text-center'>use this</div>
                         </div>
                     </div>
-                    <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
         </div>
